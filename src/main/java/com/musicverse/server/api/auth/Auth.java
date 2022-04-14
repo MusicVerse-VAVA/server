b926fe93484cf4d6fb2015d70e900d2af0bf9520
Róbert Poór <10374444+rpoor@users.noreply.github.com>
2022-04-14 19:00:17 +0200

    respond with username in auth endpoint if successful.

--- src/main/java/com/musicverse/server/api/auth/Auth.java
+++ src/main/java/com/musicverse/server/api/auth/Auth.java
@@ -1,13 +1,12 @@
 package com.musicverse.server.api.auth;
 
+import com.falsepattern.json.node.ObjectNode;
 import com.musicverse.server.HttpHelper;
 import com.musicverse.server.Util;
 import com.musicverse.server.api.GETRequestHandler;
 import com.musicverse.server.db.Database;
 import com.sun.net.httpserver.HttpExchange;
 import lombok.val;
-
-import java.sql.ResultSet;
 
 public class Auth extends GETRequestHandler {
     private static final String authQuery = Util.loadResource("/com/musicverse/server/sql/authenticate.sql");
@@ -22,12 +21,21 @@ public class Auth extends GETRequestHandler {
         val request = HttpHelper.parseJSON(exchange);
         val email = request.getString("email");
         val password = Util.hashText(request.getString("password"));
-        boolean detailsValid = db.query(authQuery, (ps) -> {
+        String username = db.query(authQuery, (ps) -> {
             ps.setString(1, email);
             ps.setString(2, password);
-        }, ResultSet::next);
-        if (detailsValid) {
-            HttpHelper.respondWithOk(exchange);
+        }, (rs) -> {
+            if (!rs.next()) {
+                return null;
+            } else {
+                return rs.getString("nickname");
+            }
+        });
+        if (username != null) {
+            val response = new ObjectNode();
+            response.set("status", "ok");
+            response.set("username", username);
+            HttpHelper.respondWithJson(exchange, 200, response);
         } else {
             HttpHelper.respondWithErrorString(exchange, 403, "Invalid email or password");
         }
