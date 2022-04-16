2677f8a6c50f959d2ce79feea10be2c111c5a0d4
Peter Čajka <cajkapeterml@gmail.com>
2022-04-16 23:36:10 +0200

    public playlists

--- src/main/java/com/musicverse/server/api/auth/Playlists.java
+++ src/main/java/com/musicverse/server/api/auth/Playlists.java
@@ -16,42 +16,76 @@ public class Playlists extends POSTRequestHandler {
 
     private static final String getPlaylistsQuery = Util.loadResource("/com/musicverse/server/sql/getPlaylists.sql");
 
+    private static final String getAllPlaylistsQuery = Util.loadResource("/com/musicverse/server/sql/getAllPlaylists.sql");
+
     public Playlists(Database db) {
         super(db);
     }
 
     @Override
     public boolean handlePostRequest(String url, String params, HttpExchange exchange) throws Throwable {
-        if (!"/playlists".equals(url)) return false;
-        val request = HttpHelper.parseJSON(exchange);
-        val id = request.getInt("id");
+        if (!"/playlists".equals(url) && ! "/allplaylists".equals(url)) return false;
+        if("/playlists".equals(url)){
+            val request = HttpHelper.parseJSON(exchange);
+            val id = request.getInt("id");
 
-        val playlists = db.query(getPlaylistsQuery, (ps) -> {
-            ps.setInt(1,id);
-        }, (rs) -> {
-            val allPlaylists = new ListNode();
+            val playlists = db.query(getPlaylistsQuery, (ps) -> {
+                ps.setInt(1,id);
+            }, (rs) -> {
+                val allPlaylists = new ListNode();
 
-            while (rs.next()){
-                val playlist = new ObjectNode();
-                playlist.set("name", rs.getString("name"));
-                playlist.set("id", rs.getInt("id"));
-                playlist.set("description", rs.getString("description"));
-                playlist.set("private",rs.getInt("private"));
-                allPlaylists.add(playlist);
+                while (rs.next()){
+                    val playlist = new ObjectNode();
+                    playlist.set("name", rs.getString("name"));
+                    playlist.set("id", rs.getInt("id"));
+                    playlist.set("description", rs.getString("description"));
+                    playlist.set("private",rs.getInt("private"));
+                    allPlaylists.add(playlist);
+                }
+                return allPlaylists;
+            });
+
+            if (playlists != null) {
+                val response = new ObjectNode();
+                response.set("status", "ok");
+                response.set("playlists", playlists);
+                HttpHelper.respondWithJson(exchange, 200, response);
+            } else {
+                HttpHelper.respondWithErrorString(exchange, 200, "Invalid id");
             }
-            return allPlaylists;
-        });
 
-        if (playlists != null) {
-            val response = new ObjectNode();
-            response.set("status", "ok");
-            response.set("playlists", playlists);
-            HttpHelper.respondWithJson(exchange, 200, response);
-        } else {
-            HttpHelper.respondWithErrorString(exchange, 200, "Invalid id");
+            return false;
         }
+        else if ("/allplaylists".equals(url)) {
+            val request = HttpHelper.parseJSON(exchange);
+            val playlists = db.query(getAllPlaylistsQuery, (ps) -> {
+            }, (rs) -> {
+                val allPlaylists = new ListNode();
 
-        return false;
+                while (rs.next()){
+                    val playlist = new ObjectNode();
+                    playlist.set("name", rs.getString("name"));
+                    playlist.set("id", rs.getInt("id"));
+                    playlist.set("description", rs.getString("description"));
+                    playlist.set("private",rs.getInt("private"));
+                    allPlaylists.add(playlist);
+                }
+                return allPlaylists;
+            });
+
+            if (playlists != null) {
+                val response = new ObjectNode();
+                response.set("status", "ok");
+                response.set("playlists", playlists);
+                HttpHelper.respondWithJson(exchange, 200, response);
+            } else {
+                HttpHelper.respondWithErrorString(exchange, 200, "Invalid id");
+            }
+
+            return false;
+        }
+        else
+            return false;
     }
 
 }
---
+++ src/main/resources/com/musicverse/server/sql/getAllPlaylists.sql
@@ -0,0 +1,1 @@
+SELECT * FROM playlists where private > 0;
